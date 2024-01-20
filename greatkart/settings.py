f78f0ee4ee17572b08773f3414cdc6462011dd8f
dhmpanchal <developer.dh.95@gmail.com>
2024-01-20 13:38:52 +0530

    change setting file update

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -14,6 +14,7 @@ import environ
 from pathlib import Path
 from django.contrib.messages import constants as messages
 from decouple import config
+from dotenv import load_dotenv
 
 env = environ.Env()
 environ.Env.read_env()
@@ -31,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -104,7 +105,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
+        'USER': config('DB_USERE'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST_NAME'),
         'PORT': config('DB_PORT'),
